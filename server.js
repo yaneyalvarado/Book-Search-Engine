9b9a6105cfe4f1274bfd1253839ab876edc69578
yaneyalvarado <alvyaney@gmail.com>
2023-07-20 10:44:58 -0600

    cloned starter code, updated index.js in models folder to export both user and book files. Also, updated line 20 in auth.jsto test to successfully function

---
+++ server.js
@@ -0,0 +1,7 @@
+// received this code from https://www.apollographql.com/docs/apollo-server/v3/migration/#nodejs
+// via canvas student website
+
+// import { createServer } from 'http';
+// import { execute, subscribe } from 'graphql';
+// import { SubscriptionServer } from 'subscriptions-transport-ws';
+// import { makeExecutableSchema } from '@graphql-tools/schema';
